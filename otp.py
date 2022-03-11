25774278df5ff865218769f0ac29680764e687c5
Ramyabhat99 <98072159+Ramyabhat99@users.noreply.github.com>
2022-03-11 09:26:43 +0530

    Delete otp.py

--- otp.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-def otpgeneration():
-    otp=""
-    for i in range(4):
-        otp+=str(random.randint(1,9))
-    print ("Your One Time Password is ")
-    print (otp)
-otpgeneration()
