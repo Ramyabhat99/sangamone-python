f0a162e099284955a3bdc9353db411eabb0dfd96
Ramyabhat99 <98072159+Ramyabhat99@users.noreply.github.com>
2022-03-26 17:24:37 +0530

    Add files via upload

---
+++ numbertowords.py
@@ -0,0 +1,12 @@
+from num2words import num2words
+
+# Most common usage.
+print(num2words(36))
+
+# Other variants, according to the type of article.
+print(num2words(36, to = 'ordinal'))
+print(num2words(36, to = 'ordinal_num'))
+print(num2words(36, to = 'year'))
+
+
+
---
+++ roundoff.py
@@ -0,0 +1,6 @@
+number=15.678
+
+# round the number
+rounded_number = round(number,1)
+print(rounded_number)
+
